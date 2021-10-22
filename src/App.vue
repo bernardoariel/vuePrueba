0ab53fcf8d421628c207853f5763b1a4f7248caa
Ariel Bernardo <arielbernardo@hotmail.com>
2021-10-22 12:06:42 -0300

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-field">
+    <label for="fieldId">Label</label>
+    <InputText id="fieldId" type="text" />
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import InputText from 'primevue/inputtext';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputText
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+const app = createApp(App);
+app.use(PrimeVue);
+import 'primeflex/primeflex.css';
+
 
 createApp(App).mount('#app')
+
